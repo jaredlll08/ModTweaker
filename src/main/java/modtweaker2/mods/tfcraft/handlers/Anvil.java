726dda30c912b16033641a94c8f0176ab737ee75
3TUSK <urey.s.knowledge@gmail.com>
2015-04-17 21:00:22 -0400

    Add loom and fire pit support
    Also minor tweak

--- src/main/java/modtweaker2/mods/tfcraft/handlers/Anvil.java
+++ src/main/java/modtweaker2/mods/tfcraft/handlers/Anvil.java
@@ -1,6 +1,7 @@
 package modtweaker2.mods.tfcraft.handlers;
 
 import static modtweaker2.helpers.InputHelper.toStack;
+import static com.bioxx.tfc.api.Crafting.AnvilReq.getReqFromInt;
 
 import java.util.ArrayList;
 
@@ -16,20 +17,22 @@ import stanhebben.zenscript.annotations.ZenMethod;
 import com.bioxx.tfc.api.Crafting.AnvilManager;
 import com.bioxx.tfc.api.Crafting.AnvilRecipe;
 
+
 @ZenClass("mods.tfcraft.Anvil")
 public class Anvil {
 	
 	@ZenMethod
-	public static void add(IItemStack output, IItemStack input1, IItemStack input2, String plan, int value, boolean flux, int req){
+	public static void add(IItemStack input1, IItemStack input2, String plan, int value, boolean flux, int req, IItemStack output){
 		MineTweakerAPI.apply(new Add(new AnvilRecipe(toStack(input1), toStack(input2), plan, value, flux, req, toStack(output))));
 	}
 	
 	/**
-	 * For weld recipe, <code>flux == true</code>
+	 * For parameter <code>req</code>:<p> 1==copper;<p> 2==bronze;<p> 3==Wrought Iron;<p>
+	 * 4==Steel;<p> 5==Black Steel;<p> 6==Red Steel;<p> 7==Blue Steel;<p> default is stone.
 	 */
 	@ZenMethod
-	public static void addWeld(IItemStack output, IItemStack input1, IItemStack input2, String plan, int value, int req){
-		MineTweakerAPI.apply(new AddWeld(new AnvilRecipe(toStack(input1), toStack(input2), plan, value, true, req, toStack(output))));
+	public static void addWeld(IItemStack input1, IItemStack input2, int req, IItemStack output){
+		MineTweakerAPI.apply(new AddWeld(new AnvilRecipe(toStack(input1), toStack(input2), true, getReqFromInt(req), toStack(output))));
 	}
 	
 	@ZenMethod
