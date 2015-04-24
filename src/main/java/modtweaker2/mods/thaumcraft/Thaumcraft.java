72eab097c81b17d532743f25487fe8570729eea6
jaredlll08 <jaredlll08@gmail.com>
2015-04-24 23:36:03 +0200

    Thaumcraft loot Bags!

--- src/main/java/modtweaker2/mods/thaumcraft/Thaumcraft.java
+++ src/main/java/modtweaker2/mods/thaumcraft/Thaumcraft.java
@@ -7,15 +7,17 @@ import modtweaker2.mods.thaumcraft.handlers.Crucible;
 import modtweaker2.mods.thaumcraft.handlers.Infusion;
 import modtweaker2.mods.thaumcraft.handlers.Research;
 import modtweaker2.mods.thaumcraft.handlers.Warp;
+import modtweaker2.mods.thaumcraft.handlers.Loot;
 
 public class Thaumcraft {
-    public Thaumcraft() {
-        MineTweakerAPI.registerClass(Arcane.class);
-        MineTweakerAPI.registerClass(Aspects.class);
-        MineTweakerAPI.registerClass(Crucible.class);
-        MineTweakerAPI.registerClass(Infusion.class);
-        MineTweakerAPI.registerClass(Research.class);
-        MineTweakerAPI.registerClass(Warp.class);
+	public Thaumcraft() {
+		MineTweakerAPI.registerClass(Arcane.class);
+		MineTweakerAPI.registerClass(Aspects.class);
+		MineTweakerAPI.registerClass(Crucible.class);
+		MineTweakerAPI.registerClass(Infusion.class);
+		MineTweakerAPI.registerClass(Research.class);
+		MineTweakerAPI.registerClass(Warp.class);
+		MineTweakerAPI.registerClass(Loot.class);
 
-    }
+	}
 }
