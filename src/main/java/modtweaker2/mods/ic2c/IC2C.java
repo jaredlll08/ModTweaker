b6f24781f1a4049a2669e974250cf9b83b39f2b6
jaredlll08 <jaredlll08@gmail.com>
2015-08-17 00:03:49 +0200

    IC2C main class

--- src/main/java/modtweaker2/mods/ic2c/IC2C.java
+++ src/main/java/modtweaker2/mods/ic2c/IC2C.java
@@ -1,11 +1,32 @@
 package modtweaker2.mods.ic2c;
 
 import minetweaker.MineTweakerAPI;
+import modtweaker2.mods.ic2c.mods.BlastFurnace;
+import modtweaker2.mods.ic2c.mods.BlockCutter;
+import modtweaker2.mods.ic2c.mods.Centrifuge;
+import modtweaker2.mods.ic2c.mods.Compressor;
+import modtweaker2.mods.ic2c.mods.Extractor;
 import modtweaker2.mods.ic2c.mods.Macerator;
+import modtweaker2.mods.ic2c.mods.MetalFormerCutting;
+import modtweaker2.mods.ic2c.mods.MetalFormerExtruding;
+import modtweaker2.mods.ic2c.mods.MetalFormerRolling;
+import modtweaker2.mods.ic2c.mods.OreWashing;
+import modtweaker2.mods.ic2c.mods.Recycler;
 
 public class IC2C {
 
 	public IC2C() {
+		MineTweakerAPI.registerClass(BlastFurnace.class);
+		MineTweakerAPI.registerClass(BlockCutter.class);
+		MineTweakerAPI.registerClass(Centrifuge.class);
+		MineTweakerAPI.registerClass(Compressor.class);
+		MineTweakerAPI.registerClass(Extractor.class);
 		MineTweakerAPI.registerClass(Macerator.class);
+		MineTweakerAPI.registerClass(MetalFormerCutting.class);
+		MineTweakerAPI.registerClass(MetalFormerExtruding.class);
+		MineTweakerAPI.registerClass(MetalFormerRolling.class);
+		MineTweakerAPI.registerClass(OreWashing.class);
+		MineTweakerAPI.registerClass(Recycler.class);
+
 	}
 }
