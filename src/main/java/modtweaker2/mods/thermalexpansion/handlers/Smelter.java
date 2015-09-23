4607835f4b6a7e64c3a40ae4629bbd1905f61727
Yulife <YulifeGaming@t-online.de>
2015-09-23 17:49:30 +0200

    Update Smelter.java

--- src/main/java/modtweaker2/mods/thermalexpansion/handlers/Smelter.java
+++ src/main/java/modtweaker2/mods/thermalexpansion/handlers/Smelter.java
@@ -99,7 +99,7 @@ public class Smelter {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@ZenMethod
-	public static void removeRecipe(IIngredient input1, IIngredient input2) {
+	public static void removeRecipe(IIngredient input2, IIngredient input2IIngredient input1) {
 	    List<RecipeSmelter> recipes = new LinkedList<RecipeSmelter>();
 	    
         for(RecipeSmelter recipe : SmelterManager.getRecipeList()) {
