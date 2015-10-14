4682dba3e6257a48d91fa5f35c1df3b16b030ab1
Tobias Wohlfarth <tobias.wohlfarth@googlemail.com>
2015-10-14 23:04:12 +0200

    Update mod dependency info for Forestry 4, thanks @mezz

--- src/main/java/modtweaker2/ModProps.java
+++ src/main/java/modtweaker2/ModProps.java
@@ -5,5 +5,5 @@ public class ModProps {
 	public static final String NAME = "Mod Tweaker 2", name = NAME;
 	public static final String MODID = "modtweaker2", modid = MODID;
 	public static final String VERSION = "0.9.4", version = VERSION;
-	public static final String DEPENDENCIES = "required-after:MineTweaker3", dependencies = DEPENDENCIES;
+	public static final String DEPENDENCIES = "required-after:MineTweaker3;after:Forestry@[4.0.3,);", dependencies = DEPENDENCIES;
 }
