b066de43f5cb119f2e8928ac13ab365ea23edcfe
Jared <jaredlll08@gmail.com>
2016-10-18 13:09:08 +0200

    Fixed drying registry

--- src/main/java/modtweaker/mods/tconstruct/TConstructHelper.java
+++ src/main/java/modtweaker/mods/tconstruct/TConstructHelper.java
@@ -21,6 +21,8 @@ public class TConstructHelper {
     public static List<ICastingRecipe> basinCasting = null;
     public static List<ICastingRecipe> tableCasting = null;
     public static List<MeltingRecipe> smeltingList = null;
+    public static List<DryingRecipe> dryingList = null;
+
     //        public static Map<ItemMetaWrapper, Integer> temperatureList = null;
     public static List<FluidStack> fuelList = new ArrayList<FluidStack>();
     public static Map<String, IModifier> modifiers = new THashMap<String, IModifier>();
@@ -30,12 +32,12 @@ public class TConstructHelper {
         try {
             alloys = ReflectionHelper.getStaticObject(TinkerRegistry.class, "alloyRegistry");
             smeltingList = ReflectionHelper.getStaticObject(TinkerRegistry.class, "meltingRegistry");
+            dryingList = ReflectionHelper.getStaticObject(TinkerRegistry.class, "dryingRegistry");
             basinCasting = ReflectionHelper.getStaticObject(TinkerRegistry.class, "basinCastRegistry");
             tableCasting = ReflectionHelper.getStaticObject(TinkerRegistry.class, "tableCastRegistry");
             modifiers = ReflectionHelper.getStaticObject(TinkerRegistry.class, "modifiers");
             modifiers_clone = new THashMap<String, IModifier>(modifiers);
             fuelList = new ArrayList<>(TinkerRegistry.getSmelteryFuels());
-
         } catch (Exception e) {
         }
     }
