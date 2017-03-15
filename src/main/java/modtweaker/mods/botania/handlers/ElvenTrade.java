f4fb5a830f444d0feaa148382acb6d75767ddf37
Jared <jaredlll08@gmail.com>
2017-03-15 21:51:41 +0200

    Close #442

--- src/main/java/modtweaker/mods/botania/handlers/ElvenTrade.java
+++ src/main/java/modtweaker/mods/botania/handlers/ElvenTrade.java
@@ -1,18 +1,20 @@
 package modtweaker.mods.botania.handlers;
 
-import com.blamejared.mtlib.helpers.LogHelper;
+import com.blamejared.mtlib.helpers.*;
 import com.blamejared.mtlib.utils.BaseListAddition;
 import com.blamejared.mtlib.utils.BaseListRemoval;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.IIngredient;
 import minetweaker.api.item.IItemStack;
+import minetweaker.mc1102.item.MCItemStack;
+import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.recipe.RecipeElvenTrade;
+import vazkii.botania.client.integration.jei.elventrade.ElvenTradeRecipeWrapper;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static com.blamejared.mtlib.helpers.InputHelper.toObjects;
 import static com.blamejared.mtlib.helpers.InputHelper.toStacks;
@@ -27,7 +29,7 @@ public class ElvenTrade {
     
     @ZenMethod
     public static void addRecipe(IItemStack[] outputs, IIngredient[] input) {
-        MineTweakerAPI.apply(new Add(new RecipeElvenTrade(toStacks(outputs), toObjects(input))));
+        MineTweakerAPI.apply(new Add(new RecipeElvenTrade(InputHelper.toStacks(outputs), toObjects(input))));
     }
 
     private static class Add extends BaseListAddition<RecipeElvenTrade> {
@@ -35,7 +37,23 @@ public class ElvenTrade {
             super(ElvenTrade.name, BotaniaAPI.elvenTradeRecipes);
             recipes.add(recipe);
         }
-
+    
+        @Override
+        public void apply() {
+            super.apply();
+            successful.forEach(rec ->{
+                MineTweakerAPI.getIjeiRecipeRegistry().addRecipe(new ElvenTradeRecipeWrapper(rec));
+            });
+        }
+        
+        @Override
+        public void undo() {
+            super.apply();
+            successful.forEach(rec ->{
+                MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(new ElvenTradeRecipeWrapper(rec));
+            });
+        }
+    
         @Override
         public String getRecipeInfo(RecipeElvenTrade recipe) {
             return LogHelper.getStackDescription(recipe.getOutputs());
@@ -50,7 +68,7 @@ public class ElvenTrade {
         LinkedList<RecipeElvenTrade> recipes = new LinkedList<RecipeElvenTrade>();
         
         for(RecipeElvenTrade entry : BotaniaAPI.elvenTradeRecipes) {
-            if(entry != null && entry.getOutputs() != null && matches(output, toStacks((IIngredient[]) entry.getOutputs().toArray()))) {
+            if(entry != null && entry.getOutputs() != null && matches(output, toStacks(entry.getOutputs().toArray(new ItemStack[entry.getOutputs().size()])))) {
                 recipes.add(entry);
             }
         }
@@ -61,16 +79,41 @@ public class ElvenTrade {
         } else {
             LogHelper.logWarning(String.format("No %s Recipe found for %s. Command ignored!", ElvenTrade.name, output.toString()));
         }
+        
+    }
+    public static IItemStack[] toStacks(ItemStack[] iIngredient) {
+        ArrayList<IItemStack> stacks = new ArrayList<>();
+    
+        for(ItemStack stack : iIngredient) {
+            stacks.add(new MCItemStack(stack));
+        }
+        
+        return stacks.toArray(new IItemStack[stacks.size()]);
     }
 
     private static class Remove extends BaseListRemoval<RecipeElvenTrade> {
         public Remove(List<RecipeElvenTrade> recipes) {
             super(ElvenTrade.name, BotaniaAPI.elvenTradeRecipes, recipes);
         }
-
+    
+        @Override
+        public void apply() {
+            super.apply();
+            successful.forEach(rec ->{
+                MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(new ElvenTradeRecipeWrapper(rec));
+            });
+        }
+    
+        @Override
+        public void undo() {
+            super.apply();
+            successful.forEach(rec ->{
+                MineTweakerAPI.getIjeiRecipeRegistry().addRecipe(new ElvenTradeRecipeWrapper(rec));
+            });
+        }
+    
         @Override
         public String getRecipeInfo(RecipeElvenTrade recipe) {
-            //TODO update logging
             return LogHelper.getStackDescription(recipe.getOutputs());
         }
     }
