bd57e409c6e49c8b9bc1347d394f7ad742b07383
Jared <jaredlll08@gmail.com>
2017-04-28 01:32:11 +0200

    Close #461

--- src/main/java/modtweaker/mods/bloodmagic/handlers/AlchemyTable.java
+++ src/main/java/modtweaker/mods/bloodmagic/handlers/AlchemyTable.java
@@ -6,9 +6,12 @@ import com.blamejared.mtlib.helpers.LogHelper;
 import com.blamejared.mtlib.utils.*;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.*;
+import modtweaker.JEIAddonPlugin;
 import modtweaker.mods.bloodmagic.BloodMagicHelper;
+import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.*;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 import static com.blamejared.mtlib.helpers.InputHelper.*;
@@ -76,7 +79,31 @@ public class AlchemyTable {
 					if(!list.remove(recipe)) {
 						LogHelper.logError(String.format("Error removing %s Recipe for %s", name, this.getRecipeInfo(recipe)));
 					} else {
-						MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(new AlchemyTableRecipeJEI(recipe));
+                        List<AlchemyTableRecipeJEI> wrappers = JEIAddonPlugin.recipeRegistry.getRecipeWrappers(JEIAddonPlugin.recipeRegistry.getRecipeCategories(Arrays.asList("BloodMagic:salchemyTable")).get(0));
+                        final AlchemyTableRecipeJEI[] recipeToRemove = {null};
+                        wrappers.forEach(wrap -> {
+                            ArrayList<Object> wrapperStacks = wrap.getRecipe().getInput();
+                            int size = recipe.getInput().size();
+                            if(wrapperStacks.size() == size) {
+                                boolean valid = true;
+                                for(int i = 0; i < size; i++) {
+                                    if(wrapperStacks.get(i) instanceof ItemStack) {
+                                        ItemStack wrapperStack = (ItemStack) wrapperStacks.get(i);
+                                        ItemStack recipeStack = (ItemStack) recipe.getInput().get(i);
+                                        if(!wrapperStack.isItemEqual(recipeStack)) {
+                                            valid = false;
+                                        }
+                                    } else {
+                                        valid = false;
+                                    }
+                                }
+                                if(valid) {
+                                    recipeToRemove[0] = wrap;
+                                }
+                            }
+                            
+                        });
+                        MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(recipeToRemove[0]);
 					}
 				} else {
 					LogHelper.logError(String.format("Error removing %s Recipe: null object", name));
@@ -91,60 +118,104 @@ public class AlchemyTable {
 	}
 	
 	@ZenMethod
-	public static void removeRecipe(IIngredient output) {
-		remove(output, BloodMagicHelper.alchemyTableList);
-	}
-	
-	public static void remove(IIngredient output, List<AlchemyTableRecipe> list) {
-		if(output == null) {
-			LogHelper.logError(String.format("Required parameters missing for %s Recipe.", name));
-			return;
-		}
-		
-		List<AlchemyTableRecipe> recipes = new LinkedList<>();
-		
-		for(AlchemyTableRecipe recipe : list) {
-			if(matches(output, toIItemStack(recipe.getRecipeOutput(null))))
-				recipes.add(recipe);
-		}
-		
-		if(!recipes.isEmpty()) {
-			MineTweakerAPI.apply(new Remove(list, recipes));
-		} else {
-			LogHelper.logWarning(String.format("No %s Recipe found for output %s. Command ignored!", AlchemyTable.name, output.toString()));
-		}
-		
-	}
-	
-	private static class Remove extends BaseListRemoval<AlchemyTableRecipe> {
-		
-		public Remove(List<AlchemyTableRecipe> list, List<AlchemyTableRecipe> recipes) {
-			super(AlchemyTable.name, list, recipes);
-		}
-		
-		@Override
-		public void apply() {
-			if (recipes.isEmpty()) {
-				return;
-			}
-			for (AlchemyTableRecipe recipe : this.recipes) {
-				if (recipe != null) {
-					if (this.list.remove(recipe)) {
-						
-						successful.add(recipe);
-						MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(new AlchemyTableRecipeJEI(recipe));
-					} else {
-						LogHelper.logError(String.format("Error removing %s Recipe for %s", name, getRecipeInfo(recipe)));
-					}
-				} else {
-					LogHelper.logError(String.format("Error removing %s Recipe: null object", name));
-				}
-			}
-		}
-		
-		@Override
-		public void undo() {
-			if (successful.isEmpty()) {
+    public static void removeRecipe(IIngredient output) {
+        remove(output, BloodMagicHelper.alchemyTableList);
+    }
+    
+    public static void remove(IIngredient output, List<AlchemyTableRecipe> list) {
+        if(output == null) {
+            LogHelper.logError(String.format("Required parameters missing for %s Recipe.", name));
+            return;
+        }
+        
+        List<AlchemyTableRecipe> recipes = new LinkedList<>();
+        
+        for(AlchemyTableRecipe recipe : list) {
+            ItemStack stack = null;
+            try {
+                stack = (ItemStack) getField(recipe.getClass(), "output").get(recipe);
+            } catch(IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            if(matches(output, toIItemStack(stack))) {
+                recipes.add(recipe);
+            }
+        }
+        
+        if(!recipes.isEmpty()) {
+            MineTweakerAPI.apply(new Remove(list, recipes));
+        } else {
+            LogHelper.logWarning(String.format("No %s Recipe found for output %s. Command ignored!", AlchemyTable.name, output.toString()));
+        }
+        
+    }
+    
+    public static Field getField(Class<?> clazz, String name) {
+        Field field = null;
+        while(clazz != null && field == null) {
+            try {
+                field = clazz.getDeclaredField(name);
+            } catch(Exception e) {
+            }
+            clazz = clazz.getSuperclass();
+        }
+        if(field != null)
+            field.setAccessible(true);
+        return field;
+    }
+    
+    private static class Remove extends BaseListRemoval<AlchemyTableRecipe> {
+        
+        public Remove(List<AlchemyTableRecipe> list, List<AlchemyTableRecipe> recipes) {
+            super(AlchemyTable.name, list, recipes);
+        }
+        
+        @Override
+        public void apply() {
+            if(recipes.isEmpty()) {
+                return;
+            }
+            for(AlchemyTableRecipe recipe : this.recipes) {
+                if(recipe != null) {
+                    if(this.list.remove(recipe)) {
+                        successful.add(recipe);
+                        List<AlchemyTableRecipeJEI> wrappers = JEIAddonPlugin.recipeRegistry.getRecipeWrappers(JEIAddonPlugin.recipeRegistry.getRecipeCategories(Arrays.asList("BloodMagic:salchemyTable")).get(0));
+                        final AlchemyTableRecipeJEI[] recipeToRemove = {null};
+                        wrappers.forEach(wrap -> {
+                            ArrayList<Object> wrapperStacks = wrap.getRecipe().getInput();
+                            int size = recipe.getInput().size();
+                            if(wrapperStacks.size() == size) {
+                                boolean valid = true;
+                                for(int i = 0; i < size; i++) {
+                                    if(wrapperStacks.get(i) instanceof ItemStack) {
+                                        ItemStack wrapperStack = (ItemStack) wrapperStacks.get(i);
+                                        ItemStack recipeStack = (ItemStack) recipe.getInput().get(i);
+                                        if(!wrapperStack.isItemEqual(recipeStack)) {
+                                            valid = false;
+                                        }
+                                    } else {
+                                        valid = false;
+                                    }
+                                }
+                                if(valid) {
+                                    recipeToRemove[0] = wrap;
+                                }
+                            }
+                            
+                        });
+                        MineTweakerAPI.getIjeiRecipeRegistry().removeRecipe(recipeToRemove[0]);
+                    } else {
+                        LogHelper.logError(String.format("Error removing %s Recipe for %s", name, getRecipeInfo(recipe)));
+                    }
+                } else {
+                    LogHelper.logError(String.format("Error removing %s Recipe: null object", name));
+                }
+            }
+        }
+        
+        @Override
+        public void undo() {
+            if (successful.isEmpty()) {
 				return;
 			}
 			for (AlchemyTableRecipe recipe : successful) {
