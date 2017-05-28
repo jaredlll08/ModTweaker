594805ee969ebd84edbee8b2df2ca48aff5b2608
Jaredlll08 <jaredlll08@gmail.com>
2017-05-28 02:04:21 +0200

    Starting anew

---
+++ src/main/java/com/blamejared/ModTweaker.java
@@ -0,0 +1,25 @@
+package com.blamejared;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.*;
+
+import static com.blamejared.reference.Reference.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION, dependencies = DEPENDENCIES)
+public class ModTweaker {
+    
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+    
+    }
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+    
+    }
+    
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+    
+    }
+}
---
+++ src/main/java/com/blamejared/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.blamejared.reference;
+
+public class Reference {
+    
+    public static final String MODID = "modtweaker";
+    public static final String NAME = "Modtweaker";
+    public static final String VERSION = "1.0.0";
+    public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mtlib;";
+}
