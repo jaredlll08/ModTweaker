63fab91cada43c69d2e0d01180c96d3171abaabe
Jared <jaredlll08@gmail.com>
2017-12-28 19:16:04 +0200

    Added support for Alchemical Imbuer and Arcane Ensorcellator (Closes #570)

---
+++ src/main/java/com/blamejared/compat/thermalexpansion/Enchanter.java
@@ -0,0 +1,79 @@
+package com.blamejared.compat.thermalexpansion;
+
+import cofh.thermalexpansion.util.managers.machine.EnchanterManager;
+import com.blamejared.ModTweaker;
+import com.blamejared.mtlib.helpers.*;
+import com.blamejared.mtlib.utils.BaseUndoable;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.annotations.*;
+import crafttweaker.api.item.IItemStack;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.*;
+
+@ZenClass("mods.thermalexpansion.Enchanter")
+@ModOnly("thermalexpansion")
+@ZenRegister
+public class Enchanter {
+    
+    @ZenMethod
+    public static void addRecipe(IItemStack output, IItemStack input, IItemStack secondInput, int energy, int experience, boolean empowered) {
+        ModTweaker.LATE_ADDITIONS.add(new Add(InputHelper.toStack(output), InputHelper.toStack(input), InputHelper.toStack(secondInput), energy, experience, empowered ? EnchanterManager.Type.EMPOWERED : EnchanterManager.Type.STANDARD));
+    }
+    
+    @ZenMethod
+    public static void removeRecipe(IItemStack input, IItemStack secondInput) {
+        ModTweaker.LATE_REMOVALS.add(new Remove(InputHelper.toStack(input), InputHelper.toStack(secondInput)));
+    }
+    
+    private static class Add extends BaseUndoable {
+        
+        private ItemStack output, input, inputSecondary;
+        private int energy, experience;
+        private EnchanterManager.Type type;
+        
+        public Add(ItemStack output, ItemStack input, ItemStack inputSecondary, int energy, int experience, EnchanterManager.Type type) {
+            super("Enchanter");
+            this.output = output;
+            this.input = input;
+            this.inputSecondary = inputSecondary;
+            this.energy = energy;
+            this.experience = experience;
+            this.type = type;
+        }
+        
+        @Override
+        public void apply() {
+            EnchanterManager.addRecipe(energy, input, inputSecondary, output, experience, type);
+        }
+        
+        @Override
+        protected String getRecipeInfo() {
+            return LogHelper.getStackDescription(input) + " and " + LogHelper.getStackDescription(inputSecondary);
+        }
+    }
+    
+    private static class Remove extends BaseUndoable {
+        
+        private ItemStack input, inputSecondary;
+        
+        public Remove(ItemStack input, ItemStack inputSecondary) {
+            super("Enchanter");
+            this.input = input;
+            this.inputSecondary = inputSecondary;
+        }
+        
+        @Override
+        public void apply() {
+            if(!EnchanterManager.recipeExists(input, inputSecondary)) {
+                CraftTweakerAPI.logError("No Enchanter recipe exists for: " + input + " and " + inputSecondary);
+                return;
+            }
+            EnchanterManager.removeRecipe(input, inputSecondary);
+        }
+        
+        @Override
+        protected String getRecipeInfo() {
+            return LogHelper.getStackDescription(input) + " and " + LogHelper.getStackDescription(inputSecondary);
+        }
+    }
+}
---
+++ src/main/java/com/blamejared/compat/thermalexpansion/Imbuer.java
@@ -0,0 +1,81 @@
+package com.blamejared.compat.thermalexpansion;
+
+import cofh.thermalexpansion.util.managers.machine.BrewerManager;
+import com.blamejared.ModTweaker;
+import com.blamejared.mtlib.helpers.*;
+import com.blamejared.mtlib.utils.BaseUndoable;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.annotations.*;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.liquid.ILiquidStack;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import stanhebben.zenscript.annotations.*;
+
+@ZenClass("mods.thermalexpansion.Imbuer")
+@ModOnly("thermalexpansion")
+@ZenRegister
+public class Imbuer {
+    
+    @ZenMethod
+    public static void addRecipe(ILiquidStack output, IItemStack input, ILiquidStack inputFluid, int energy) {
+        ModTweaker.LATE_ADDITIONS.add(new Add(InputHelper.toFluid(output), InputHelper.toStack(input), InputHelper.toFluid(inputFluid), energy));
+    }
+    
+    @ZenMethod
+    public static void removeRecipe(IItemStack input, ILiquidStack secondInput) {
+        ModTweaker.LATE_REMOVALS.add(new Remove(InputHelper.toStack(input), InputHelper.toFluid(secondInput)));
+    }
+    
+    private static class Add extends BaseUndoable {
+        
+        private FluidStack output;
+        private ItemStack input;
+        private FluidStack inputFluid;
+        private int energy;
+        
+        public Add(FluidStack output, ItemStack input, FluidStack inputFluid, int energy) {
+            super("Imbuer");
+            this.output = output;
+            this.input = input;
+            this.inputFluid = inputFluid;
+            this.energy = energy;
+        }
+        
+        @Override
+        public void apply() {
+            BrewerManager.addRecipe(energy, input, inputFluid, output);
+        }
+        
+        @Override
+        protected String getRecipeInfo() {
+            return LogHelper.getStackDescription(input) + " and " + LogHelper.getStackDescription(inputFluid);
+        }
+    }
+    
+    private static class Remove extends BaseUndoable {
+        
+        private ItemStack input;
+        private FluidStack fluid;
+        
+        public Remove(ItemStack input, FluidStack fluid) {
+            super("Imbuer");
+            this.input = input;
+            this.fluid = fluid;
+        }
+        
+        @Override
+        public void apply() {
+            if(!BrewerManager.recipeExists(input, fluid)) {
+                CraftTweakerAPI.logError("No Imbuer recipe exists for: " + input + " and " + fluid);
+                return;
+            }
+            BrewerManager.removeRecipe(input, fluid);
+        }
+        
+        @Override
+        protected String getRecipeInfo() {
+            return LogHelper.getStackDescription(input) + " and " + LogHelper.getStackDescription(fluid);
+        }
+    }
+}
