f4d391453c20c875b8d789e9b58a5e05ca457fe5
Lanse <simon_stalnabb@hotmail.com>
2018-01-17 16:28:30 +0100

    Added KnowledgeType Registration and a ZenExpansion for adding Knowledge to the Lexica Botania

--- src/main/java/com/blamejared/compat/botania/handlers/Knowledge.java
+++ src/main/java/com/blamejared/compat/botania/handlers/Knowledge.java
@@ -5,9 +5,7 @@ import crafttweaker.CraftTweakerAPI;
 import crafttweaker.IAction;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.translation.LanguageMap;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import vazkii.botania.api.BotaniaAPI;
@@ -19,16 +17,13 @@ public class Knowledge {
     private static final String typeName = "Botania Knowledge Variable:";
 
     @ZenMethod
-    public static void registerKnowledgeType(String unlocalized, String localized, String color, boolean autoUnlock){
+    public static void registerKnowledgeType(String unlocalized, String color, boolean autoUnlock){
         if (unlocalized == null || unlocalized.isEmpty()) {
             CraftTweakerAPI.logError("Found null String in " + typeName + " name");
         } else if (color == null || color.isEmpty()){
             CraftTweakerAPI.logError("Found null String in " + typeName + " color");
-        } else if (localized == null || localized.isEmpty()){
-            CraftTweakerAPI.logError("Found null String in " + typeName + " localization");
         } else {
             ModTweaker.LATE_ADDITIONS.add(new Add(unlocalized, color, autoUnlock));
-            LanguageMap.instance.languageList.put("botania.knowledge." + unlocalized, localized);
         }
     }
 
