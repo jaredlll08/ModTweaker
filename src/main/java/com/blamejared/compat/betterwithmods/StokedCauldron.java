5d4a8032afa7e92ce7408971ab0b62b2eb3c3490
Jared <jaredlll08@gmail.com>
2018-02-18 23:56:15 +0200

    Once again, optioanls go at the end

--- src/main/java/com/blamejared/compat/betterwithmods/StokedCauldron.java
+++ src/main/java/com/blamejared/compat/betterwithmods/StokedCauldron.java
@@ -4,18 +4,12 @@ package com.blamejared.compat.betterwithmods;
 import betterwithmods.common.registry.bulk.manager.StokedCauldronManager;
 import betterwithmods.common.registry.bulk.recipes.StokedCauldronRecipe;
 import com.blamejared.ModTweaker;
-import com.blamejared.compat.betterwithmods.util.BulkAdd;
-import com.blamejared.compat.betterwithmods.util.BulkRemove;
+import com.blamejared.compat.betterwithmods.util.*;
 import com.blamejared.mtlib.helpers.InputHelper;
-import crafttweaker.annotations.ModOnly;
-import crafttweaker.annotations.ZenRegister;
-import crafttweaker.api.item.IIngredient;
-import crafttweaker.api.item.IItemStack;
+import crafttweaker.annotations.*;
+import crafttweaker.api.item.*;
 import net.minecraft.item.ItemStack;
-import stanhebben.zenscript.annotations.NotNull;
-import stanhebben.zenscript.annotations.Optional;
-import stanhebben.zenscript.annotations.ZenClass;
-import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.*;
 
 
 @ZenClass("mods.betterwithmods.StokedCauldron")
@@ -24,9 +18,15 @@ import stanhebben.zenscript.annotations.ZenMethod;
 public class StokedCauldron {
     
     @ZenMethod
-    public static void add(IItemStack output, @Optional IItemStack secondaryOutput, @NotNull IIngredient[] inputs) {
+    public static void add(IItemStack output, @NotNull IIngredient[] inputs, @Optional IItemStack secondaryOutput) {
         StokedCauldronRecipe r = new StokedCauldronRecipe(InputHelper.toStack(output), InputHelper.toStack(secondaryOutput), InputHelper.toObjects(inputs));
         ModTweaker.LATE_ADDITIONS.add(new BulkAdd("Set Stoked Cauldron Recipe", StokedCauldronManager.getInstance(), r));
+    }
+    
+    @ZenMethod
+    @Deprecated
+    public static void add(IItemStack output, @Optional IItemStack secondaryOutput, @NotNull IIngredient[] inputs) {
+        add(output, inputs, secondaryOutput);
     }
     
     @ZenMethod
