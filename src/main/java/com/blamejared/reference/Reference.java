adf8cbda0cfd7dd43e197c1f7fd93ce986b77582
Jared <jaredlll08@gmail.com>
2018-03-07 11:42:45 +0200

    version push

--- src/main/java/com/blamejared/reference/Reference.java
+++ src/main/java/com/blamejared/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     
     public static final String MODID = "modtweaker";
     public static final String NAME = "Modtweaker";
-    public static final String VERSION = "4.0.7";
+    public static final String VERSION = "4.0.8";
     public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mtlib;before:jei";
 }
