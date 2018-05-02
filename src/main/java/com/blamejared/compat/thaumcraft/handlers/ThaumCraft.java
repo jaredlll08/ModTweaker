8abe439e7a9f6f519a22adcac533bd88a9e7df2a
modmuss50 <modmuss50@gmail.com>
2018-05-02 17:04:33 +0100

    Fix crash when thaumcraft is not installed

--- src/main/java/com/blamejared/compat/thaumcraft/handlers/ThaumCraft.java
+++ src/main/java/com/blamejared/compat/thaumcraft/handlers/ThaumCraft.java
@@ -1,11 +1,12 @@
 package com.blamejared.compat.thaumcraft.handlers;
 
 import com.blamejared.compat.thaumcraft.handlers.brackets.BracketHandlerAspect;
+import crafttweaker.annotations.ModOnly;
 import crafttweaker.zenscript.GlobalRegistry;
 
 public class ThaumCraft {
-    
-    
+
+	@ModOnly("thaumcraft")
     public static void preInit(){
         GlobalRegistry.registerBracketHandler(new BracketHandlerAspect());
     }
